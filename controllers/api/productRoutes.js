db052deb860c0af22af201035fe0e4666f152dfd
Rajveer Singh <rajsidhu2924@gmail.com>
2022-04-11 11:56:26 -0700

    get route added

--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -1,6 +1,20 @@
 const router = require("express").Router();
 const { Product } = require("../../models");
 
+router.get('/', async (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  try {
+    const categoryData = await Product.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+
 router.post("/", async (req, res) => {
   try {
     const newProduct = await Product.create({
