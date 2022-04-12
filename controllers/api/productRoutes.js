84377118a0b256cc7f618108396f4fd2e9665741
Zion Yang <zionremnant@gmail.com>
2022-04-12 13:14:42 -0700

    put route zy

--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -1,14 +1,25 @@
 const router = require("express").Router();
 const { Product } = require("../../models");
 
-// get all products
 router.get("/", async (req, res) => {
-  // find all products
   try {
     const productData = await Product.findAll({});
     res.status(200).json(productData);
   } catch (error) {
     res.status(500).json(error);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {});
+    if (!productData) {
+      res.status(404).json({ message: "No product found with that id!" });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
@@ -23,6 +34,48 @@ router.post("/", async (req, res) => {
   } catch (err) {
     res.status(400).json(err);
   }
+});
+
+// update product
+router.put("/:id", (req, res) => {
+  // update product data
+  Product.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    // .then((product) => {
+    //   // find all associated tags from ProductTag
+    //   return ProductTag.findAll({ where: { product_id: req.params.id } });
+    // })
+    // .then((productTags) => {
+    //   // get list of current tag_ids
+    //   const productTagIds = productTags.map(({ tag_id }) => tag_id);
+    //   // create filtered list of new tag_ids
+    //   const newProductTags = req.body.tagIds
+    //     .filter((tag_id) => !productTagIds.includes(tag_id))
+    //     .map((tag_id) => {
+    //       return {
+    //         product_id: req.params.id,
+    //         tag_id,
+    //       };
+    //     });
+    //   // figure out which ones to remove
+    //   const productTagsToRemove = productTags
+    //     .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+    //     .map(({ id }) => id);
+
+    //   // run both actions
+    //   return Promise.all([
+    //     ProductTag.destroy({ where: { id: productTagsToRemove } }),
+    //     ProductTag.bulkCreate(newProductTags),
+    //   ]);
+    // })
+    // .then((updatedProductTags) => res.json(updatedProductTags))
+    .catch((err) => {
+      // console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.delete("/:id", async (req, res) => {
