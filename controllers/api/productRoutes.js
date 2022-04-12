223e5645d039814f0852c9bad84e4fb60ad243eb
Zion Yang <zionremnant@gmail.com>
2022-04-12 13:26:19 -0700

    put route zy

--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -14,7 +14,7 @@ router.get("/:id", async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id, {});
     if (!productData) {
-      res.status(404).json({ message: "No product found with that id!" });
+      res.status(404).json({ message: "No product found with that id" });
       return;
     }
     res.status(200).json(productData);
@@ -88,7 +88,7 @@ router.delete("/:id", async (req, res) => {
     });
 
     if (!productData) {
-      res.status(404).json({ message: "No product found with this id!" });
+      res.status(404).json({ message: "No product found with this id" });
       return;
     }
 
